4ba4239c931896400f08c87f801b861023ce7531
Kelvin <57607285+KelvinWCH@users.noreply.github.com>
2024-07-30 07:55:46 +0000

    Add files via upload

---
+++ script/buttons.js
@@ -0,0 +1,9 @@
+function go_home(){
+    console.log("test");
+    window.location.href = "./index.html";
+}
+
+function go_reading_blog(){
+    console.log("test");
+    window.location.href = "./reading-blogs.html";
+}
